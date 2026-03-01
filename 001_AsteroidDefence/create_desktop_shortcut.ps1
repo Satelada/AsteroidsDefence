dba3be6a7de1f81d5c1a344ed64e8a1b3d249bf9
Satelada <daniel@markert.in>
2026-03-01 17:28:22 +0100

    Delete 001_AsteroidDefence directory

--- 001_AsteroidDefence/create_desktop_shortcut.ps1
+++
@@ -1,8 +0,0 @@
-$WshShell = New-Object -ComObject WScript.Shell
-$Desktop = [Environment]::GetFolderPath("Desktop")
-$Shortcut = $WshShell.CreateShortcut((Join-Path $Desktop "Falling Asteroids.lnk"))
-$Shortcut.TargetPath = Join-Path $PSScriptRoot "Start_Falling_Asteroids.bat"
-$Shortcut.WorkingDirectory = $PSScriptRoot
-$Shortcut.Description = "Falling Asteroids"
-$Shortcut.Save()
-Write-Host "Desktop-Verknuepfung erstellt: $($Shortcut.FullName)"
